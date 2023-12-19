8d19cc3d3d0bc14d75e9e763f25d0b909530cccb
this_girl_shall_code <emmi.lou002@gmail.com>
2023-12-19 12:00:50 +0000

    added function to loop and add midday forecasts under 5-day forecast section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 const today = $('#today'); //selecting the section to append a div etc for today's forecast
 const forecast = $('#forecast'); // selecting the section to append a div etc to the rest of the forecast
 let storedCity = [];
+let i = 0;
+let isToday = true;
 
 renderLocalStorage();
 
@@ -29,19 +31,38 @@ function fetchCityForecast(city){
     }).then(function(data){
         console.log(data);
 
-        let city = data.city.name;
-        let apiDate = data.list[0].dt_txt.substr(0, 10); //.substr(0, 10) keeps the first 10 characters of the string so from this: 2023-12-18 12:00:00 to this: 2023-12-18
-        let properDate = dayjs(`${apiDate}`, `YYYY-MM-DD`).format(`(DD/MM/YYYY)`); // converts the API date to a different format
-        let time = data.list[0].dt_txt.substr(11, 2); //to obtain the hour from the date text of the API - to use to show only midday forecasts
-        console.log(time);
-        console.log(data.list.length); //40
-        let weatherIcon = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
-        let wind = (data.list[0].wind.speed * 2.237).toFixed(2); //the api wind speed is meters per sec (mps) so to get MPH * 2.237
-        let temp = data.list[0].main.temp;
-        let humidity = data.list[0].main.humidity;
+        for(let i = 0; i < data.list.length; i++){
 
-        renderWeather(city, properDate, weatherIcon, temp, wind, humidity);
-        
+            let city = data.city.name;
+            let apiDate = data.list[i].dt_txt.substr(0, 10); //.substr(0, 10) keeps the first 10 characters of the string so from this: 2023-12-18 12:00:00 to this: 2023-12-18
+            let properDate = dayjs(`${apiDate}`, `YYYY-MM-DD`).format(`(DD/MM/YYYY)`); // converts the API date to a different format
+            let weatherIcon = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
+            let wind = (data.list[i].wind.speed * 2.237).toFixed(2); //the api wind speed is meters per sec (MPS) so to get MPH = MPS * 2.237
+            let temp = data.list[i].main.temp;
+            let humidity = data.list[i].main.humidity;
+
+            console.log(data.list.length); //40 - the amount of forecast objects
+
+            let day = data.list[i].dt_txt.substr(8, 2); //to obtain the day from the date text of the API - to use to show only single day forecasts
+            console.log(day);
+
+            let time = data.list[i].dt_txt.substr(11, 2); //to obtain the hour from the date text of the API - to use to show only midday forecasts
+            console.log(time);
+
+            if(isToday){
+                forecast.empty(); //move elsewhere?
+                today.empty(); //move elsewhere?
+                today.css('border', '1px solid black');
+                const forecast5Day = $('<h2>');
+                forecast5Day.text('5-Day Forecast:');
+                forecast.append(forecast5Day);
+                renderWeather(day, city, properDate, weatherIcon, temp, wind, humidity);
+                isToday = false;
+            }else if (!isToday && (time == 12) ){
+                renderWeather(day, city, properDate, weatherIcon, temp, wind, humidity);
+            }
+        };
+
         if(storedCity.includes(city)){
             return
         }else{
@@ -51,51 +72,42 @@ function fetchCityForecast(city){
     });
 };
 
-function renderWeather(city, properDate, weatherIcon, temp, wind, humidity){
-        forecast.empty();
-        today.empty();
-    // for(let i = 1; i < data.list.length; i++){    
-        today.css('border', '1px solid black');
+function renderWeather(day, city, properDate, weatherIcon, temp, wind, humidity){
+    
+    const newImg = $('<img>');
+    newImg.attr({'src': weatherIcon, 'id': `day-${day}-img`});
+    
+    const newTemp = $('<p>');
+    newTemp.attr('id', `day-${day}-temp`);
+    newTemp.text(`Temp: ${temp}°c`);
 
+    const newWind = $('<p>');
+    newWind.attr('id', `day-${day}-wind`);
+    newWind.text(`Wind: ${wind} MPH`);
+
+    const newHumidity = $('<p>');
+    newHumidity.attr('id', `day-${day}-humidity`);
+    newHumidity.text(`Humidity: ${humidity}%`);
+
+    if(isToday){
         const newH2 = $('<h2>');
-        newH2.attr('id', 'todays-date');
+        newH2.attr('id', `todays-date-${day}`);
         newH2.text(`${city} ${properDate}`);
-
-        const newImg = $('<img>');
-        newImg.attr('src', weatherIcon);
-        newH2.append(newImg); //add img to the header
-
-        const newTemp = $('<p>');
-        newTemp.attr('id', 'temp');
-        newTemp.text(`Temp: ${temp}°c`);
-
-        const newWind = $('<p>');
-        newWind.attr('id', 'wind');
-        newWind.text(`Wind: ${wind} MPH`);
-
-        const newHumidity = $('<p>');
-        newHumidity.attr('id', 'humidity');
-        newHumidity.text(`Humidity: ${humidity}%`);
-
+        newH2.append(newImg); //adds img to the header
         today.append(newH2, newTemp, newWind, newHumidity);
-        
-            
-        const forecast5Day = $('<h2>');
-        forecast5Day.text('5-Day Forecast:');
-
+    }else{
         const newDiv = $('<div>');
-        // newDiv.attr('id', 'date-weather');
-        newDiv.attr({'id': 'date-weather', 'class': 'col-lg-3 m-1'}); 
-        newDiv.css('background-color', 'blue');
+        newDiv.attr({'id': `day-${day}-weather`, 'class': 'col-lg-3 m-1'}); 
+        newDiv.css({'background-color': 'blue', 'color': 'white'});
 
-        const newH3 = $('<h3>');
-        newH3.attr('id', 'date-date');
-        newH3.text("I'm tomorrows date heading");
-        // newH3.append(newSpan);
+        const newH4 = $('<h4>');
+        newH4.attr('id', `date-${day}`);
+        newH4.text(`${city} ${properDate}`);
+        newH4.append(newImg); //adds img to the header
 
-        newDiv.append(newH3);
-        forecast.append(forecast5Day, newDiv);
-    // };
+        newDiv.append(newH4, newTemp, newWind, newHumidity);
+        forecast.append(newDiv);
+    };
 };
 
 /****************************** LOCAL STORAGE & HISTORY ****************************************/
@@ -126,7 +138,7 @@ $('.btn-secondary').on('click', function(event){
     fetchCityForecast(target);
 })
 
-/**********************************************************************/
+/********************************************************************************************/
 
 
 //DONE save searched city to local storage
