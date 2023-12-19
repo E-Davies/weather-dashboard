bcee0b4ab93fdeab476fb46bbf404bf9e799a3d5
this_girl_shall_code <emmi.lou002@gmail.com>
2023-12-19 16:27:48 +0000

    adjusted bootstrap properties on elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 
-const today = $('#today'); //selecting the section to append a div etc for today's forecast
-const forecast = $('#forecast'); // selecting the section to append a div etc to the rest of the forecast
+const today = $('#today'); //selecting the section to append info for today's forecast
+const forecast = $('#forecast'); // selecting the section to append a div of info for the next 5 days of forecast
 let storedCity = [];
-// let i = 0;
 let isToday = true;
 
 renderLocalStorage();
@@ -16,6 +15,9 @@ $('#search-button').on('click', function(event){
 $('#clear-button').on('click', function(event){
     event.preventDefault();
     localStorage.clear();
+    forecast.empty(); 
+    today.empty(); 
+    today.css('border', '');
     $('.history-btns').empty();
     storedCity = [];
 });
@@ -31,29 +33,38 @@ function fetchCityForecast(city){
     .then(function(response){
         return response.json();
     }).then(function(data){
-        // console.log(data);
+        console.log(data);
 
         let apiCity = data.city.name;
+
+        let currentTime = dayjs().format('HH');
+        console.log(`Current time: ${currentTime}`);
+
+        // let startAPIDate = data.list[0].dt_txt.substr(0, 10)
+        // let properDate = dayjs(`${apiDate}`, `YYYY-MM-DD`).format(`DD/MM/YYYY`);
 
         for(let i = 0; i < data.list.length; i++){
 
             let apiDate = data.list[i].dt_txt.substr(0, 10); //.substr(0, 10) keeps the first 10 characters of the string so from this: 2023-12-18 12:00:00 to this: 2023-12-18
-            let properDate = dayjs(`${apiDate}`, `YYYY-MM-DD`).format(`(DD/MM/YYYY)`); // converts the API date to a different format
+            let properDate = dayjs(`${apiDate}`, `YYYY-MM-DD`).format(`DD/MM/YYYY`); // converts the API date to a different format
             let weatherIcon = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
-            let wind = (data.list[i].wind.speed * 2.237).toFixed(2); //the api wind speed is meters per sec (MPS) so to get MPH = MPS * 2.237
+            let wind = (data.list[i].wind.speed * 2.237).toFixed(2); //the API wind speed is in meters per sec (MPS) so to get MPH = MPS * 2.237
             let temp = data.list[i].main.temp;
             let humidity = data.list[i].main.humidity;
             let day = data.list[i].dt_txt.substr(8, 2); //to obtain the day from the date text of the API - to use to add dates to id's
             let time = data.list[i].dt_txt.substr(11, 2); //to obtain the hour from the date text of the API - to use to show only midday forecasts
 
-            if(isToday){
+            if(isToday){ //this will render the forecast for todays most current time  
                 today.css('border', '1px solid black');
                 const forecast5Day = $('<h2>');
+                forecast5Day.attr('class', 'ps-0');
                 forecast5Day.text('5-Day Forecast:');
                 forecast.append(forecast5Day);
                 renderWeather(day, apiCity, properDate, weatherIcon, temp, wind, humidity);
-                isToday = false;
-            }else if (!isToday && (time == 12) ){
+                isToday = false; // changes to false so that on the next loop it will render the next date
+            // }else if(!isToday && (currentTime < 12)){
+
+            }else if (!isToday && (time == 12) ){ //this will render 12pm forecasts for other dates that aren't today's date
                 renderWeather(day, apiCity, properDate, weatherIcon, temp, wind, humidity);
             }
         };
@@ -97,7 +108,7 @@ function renderWeather(day, city, properDate, weatherIcon, temp, wind, humidity)
         today.append(newH2, newTemp, newWind, newHumidity);
     }else{
         const newDiv = $('<div>');
-        newDiv.attr({'id': `day-${day}-weather`, 'class': 'col-lg-2 mx-auto my-1 smaller-text'}); 
+        newDiv.attr({'id': `day-${day}-weather`, 'class': 'col-lg-2 mx-auto my-2 smaller-text'}); 
         newDiv.css({'background-color': '#304356', 'color': 'white', 'border-radius': '5px'});
 
         const newH5 = $('<h5>');
